8be45ce55a13f4ca1ef30029f152e43de19ae42a
daviyang35 <daviyang35@gmail.com>
2018-01-29 09:52:14 +0800

    add notify module

---
+++ notify/src/main/java/io/nuls/notify/module/NotifyModule.java
@@ -0,0 +1,79 @@
+package io.nuls.notify.module;
+
+import io.nuls.core.event.BaseEvent;
+import io.nuls.core.module.BaseModuleBootstrap;
+import io.nuls.core.utils.log.Log;
+import io.nuls.event.bus.service.impl.EventBusServiceImpl;
+import io.nuls.event.bus.service.intf.EventBusService;
+import io.nuls.notify.handler.EventsHandler;
+import io.nuls.notify.websocket.NotifyServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * @author daviyang35
+ * @date 2018/1/19
+ */
+public class NotifyModule extends BaseModuleBootstrap {
+    private EventBusService eventBusService;
+    private NotifyServer notifyServer;
+    String host;
+    short port;
+
+    public NotifyModule() {
+        super((short) 10);
+    }
+
+    @Override
+    public void init() {
+        Log.debug("Init");
+        // read property
+        eventBusService = EventBusServiceImpl.getInstance();
+        eventBusService.subscribeEvent(BaseEvent.class, new EventsHandler());
+        host = getModuleCfgProperty("notify","notify.host");
+        port = Short.parseShort(getModuleCfgProperty("notify","notify.port"));
+    }
+
+    @Override
+    public void start() {
+        Log.debug("Start");
+        // running web socket server
+        if (host== null || host.isEmpty()) {
+            Log.error("WebSocket property is missing");
+            return;
+        }
+
+        notifyServer = new NotifyServer(new InetSocketAddress(host,port));
+        notifyServer.setReuseAddr(true);
+        notifyServer.setTcpNoDelay(true);
+        notifyServer.start();
+    }
+
+    @Override
+    public void shutdown() {
+        Log.debug("Shutdown");
+        // stop web socket server
+        try {
+            notifyServer.stop();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Log.debug("Destroy");
+        notifyServer = null;
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public int getVersion() {
+        return 0;
+    }
+}
---
+++ notify/src/main/java/io/nuls/notify/websocket/NotifyServer.java
@@ -0,0 +1,42 @@
+package io.nuls.notify.websocket;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author daviyang35
+ * @date 2018/1/19
+ */
+public class NotifyServer extends WebSocketServer {
+    public NotifyServer(InetSocketAddress address) {
+        super(address);
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+
+    }
+
+    @Override
+    public void onClose(WebSocket webSocket, int i, String s, boolean b) {
+
+    }
+
+    @Override
+    public void onMessage(WebSocket webSocket, String s) {
+
+    }
+
+    @Override
+    public void onError(WebSocket webSocket, Exception e) {
+
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+}
